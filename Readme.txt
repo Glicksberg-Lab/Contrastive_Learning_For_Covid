c6f6803ef39b6dff89d4ef4c5ae4f57858f56f1c
Tingyi <tiwanyan@gmail.com>
2021-02-14 21:33:27 -0500

    polish

--- Readme.txt
+++ Readme.txt
@@ -15,16 +15,28 @@ numpy array
 columns:
 [0]:patient mrn id
 [1]:admit time:
-must be in the form of "year-day-hour".
-For example: "2020-06-03"
+must be in the form of: "2020=05-20 14:52:00"
 [2]:In ICU time, put nan if no ICU transfer
 [3]:Intubation time
 [4]:death time
 [5]:covid observation time
-[6]:visit ID
 
 arg1:Lab test:
 numpy array
+columns:
+[0]:patient mrn id
+[1]:lab name
+[2]:lab result value
+[3]:lab result time:
+In the form of:"20200520180800"
 
 arg3:Vital sign:
 numpy array
+columns:
+[0]:patient mrn id
+[1]:vital sign name
+[2]:vital sign result value
+[3]:vital sign result time:
+In the form of:"20200520180800"
+
+
--- kg_constraction_whole_new.py
+++ kg_constraction_whole_new.py
@@ -70,75 +70,73 @@ class Kg_construct_ehr():
             print("im here in filter visit ID")
             print(i)
             mrn_single = self.covid_ar[i,0]
-            visit_id = self.covid_ar[i,6]
-            if visit_id == visit_id:
-                if mrn_single not in self.dic_patient.keys():
-                    self.dic_patient[mrn_single] = {}
-                    self.dic_patient[mrn_single]['prior_time_vital'] = {}
-                    self.dic_patient[mrn_single]['prior_time_lab'] = {}
-                in_admit_time_single = self.covid_ar[i,1]
+            if mrn_single not in self.dic_patient.keys():
+                self.dic_patient[mrn_single] = {}
+                self.dic_patient[mrn_single]['prior_time_vital'] = {}
+                self.dic_patient[mrn_single]['prior_time_lab'] = {}
+            in_admit_time_single = self.covid_ar[i,1]
 
-                self.in_admit_time = in_admit_time_single.split(' ')
-                in_admit_date = [np.int(j) for j in self.in_admit_time[0].split('-')]
-                in_admit_date_value = (in_admit_date[0] * 365.0 + in_admit_date[1] * 30 + in_admit_date[2]) * 24 * 60
-                self.in_admit_time_ = [np.int(j) for j in self.in_admit_time[1].split(':')[0:-1]]
-                in_admit_time_value = self.in_admit_time_[0] * 60.0 + self.in_admit_time_[1]
-                total_in_admit_time_value = in_admit_date_value + in_admit_time_value
-                self.dic_patient[mrn_single].setdefault('Admit_time_values', []).append(total_in_admit_time_value)
-                """
-                filter intubation
-                """
-                if self.covid_ar[i, 2] == self.covid_ar[i, 2]:
-                    self.dic_patient[mrn_single]['icu_label'] = 1
-                    in_time_single = self.covid_ar[i, 2]
-                    self.in_time = in_time_single.split(' ')
-                    in_date = [np.int(j) for j in self.in_time[0].split('-')]
-                    in_date_value = (in_date[0] * 365.0 + in_date[1] * 30 + in_date[2]) * 24 * 60
-                    self.in_time_ = [np.int(j) for j in self.in_time[1].split(':')[0:-1]]
-                    in_time_value = self.in_time_[0] * 60.0 + self.in_time_[1]
-                    total_in_time_value = in_date_value + in_time_value
-                    self.dic_patient[mrn_single]['in_icu_time'] = self.in_time
-                    self.dic_patient[mrn_single]['in_date'] = in_date
-                    self.dic_patient[mrn_single]['in_time'] = self.in_time_
-                    self.dic_patient[mrn_single]['total_in_icu_time_value'] = total_in_time_value
-                else:
-                    self.dic_patient[mrn_single]['icu_label'] = 0
-                """
-                filter intubation
-                """
-                if self.covid_ar[i, 3] == self.covid_ar[i, 3]:
-                    self.dic_patient[mrn_single]['intubation_label'] = 1
-                    in_time_single = self.covid_ar[i, 3]
-                    self.in_time = in_time_single.split(' ')
-                    in_date = [np.int(i) for i in self.in_time[0].split('-')]
-                    in_date_value = (in_date[0] * 365.0 + in_date[1] * 30 + in_date[2]) * 24 * 60
-                    self.in_time_ = [np.int(i) for i in self.in_time[1].split(':')[0:-1]]
-                    in_time_value = self.in_time_[0] * 60.0 + self.in_time_[1]
-                    total_in_time_value = in_date_value + in_time_value
-                    self.dic_patient[mrn_single]['intubation_time'] = self.in_time
-                    self.dic_patient[mrn_single]['intubation_date'] = in_date
-                    self.dic_patient[mrn_single]['intubation_time'] = self.in_time_
-                    self.dic_patient[mrn_single]['total_intubation_time_value'] = total_in_time_value
-                else:
-                    self.dic_patient[mrn_single]['intubation_label'] = 0
+            self.in_admit_time = in_admit_time_single.split(' ')
+            in_admit_date = [np.int(j) for j in self.in_admit_time[0].split('-')]
+            in_admit_date_value = (in_admit_date[0] * 365.0 + in_admit_date[1] * 30 + in_admit_date[2]) * 24 * 60
+            self.in_admit_time_ = [np.int(j) for j in self.in_admit_time[1].split(':')[0:-1]]
+            in_admit_time_value = self.in_admit_time_[0] * 60.0 + self.in_admit_time_[1]
+            total_in_admit_time_value = in_admit_date_value + in_admit_time_value
+            self.dic_patient[mrn_single].setdefault('Admit_time_values', []).append(total_in_admit_time_value)
+            """
+            filter intubation
+            """
+            if self.covid_ar[i, 2] == self.covid_ar[i, 2]:
+                self.dic_patient[mrn_single]['icu_label'] = 1
+                in_time_single = self.covid_ar[i, 2]
+                self.in_time = in_time_single.split(' ')
+                in_date = [np.int(j) for j in self.in_time[0].split('-')]
+                in_date_value = (in_date[0] * 365.0 + in_date[1] * 30 + in_date[2]) * 24 * 60
+                self.in_time_ = [np.int(j) for j in self.in_time[1].split(':')[0:-1]]
+                in_time_value = self.in_time_[0] * 60.0 + self.in_time_[1]
+                total_in_time_value = in_date_value + in_time_value
+                self.dic_patient[mrn_single]['in_icu_time'] = self.in_time
+                self.dic_patient[mrn_single]['in_date'] = in_date
+                self.dic_patient[mrn_single]['in_time'] = self.in_time_
+                self.dic_patient[mrn_single]['total_in_icu_time_value'] = total_in_time_value
+            else:
+                self.dic_patient[mrn_single]['icu_label'] = 0
+            """
+            filter intubation
+            """
+            if self.covid_ar[i, 3] == self.covid_ar[i, 3]:
+                self.dic_patient[mrn_single]['intubation_label'] = 1
+                in_time_single = self.covid_ar[i, 3]
+                self.in_time = in_time_single.split(' ')
+                in_date = [np.int(i) for i in self.in_time[0].split('-')]
+                in_date_value = (in_date[0] * 365.0 + in_date[1] * 30 + in_date[2]) * 24 * 60
+                self.in_time_ = [np.int(i) for i in self.in_time[1].split(':')[0:-1]]
+                in_time_value = self.in_time_[0] * 60.0 + self.in_time_[1]
+                total_in_time_value = in_date_value + in_time_value
+                self.dic_patient[mrn_single]['intubation_time'] = self.in_time
+                self.dic_patient[mrn_single]['intubation_date'] = in_date
+                self.dic_patient[mrn_single]['intubation_time'] = self.in_time_
+                self.dic_patient[mrn_single]['total_intubation_time_value'] = total_in_time_value
+            else:
+                self.dic_patient[mrn_single]['intubation_label'] = 0
 
-                """
-                filter mortality
-                """
-                if self.covid_ar[i, 4] == self.covid_ar[i, 4]:
-                    death_flag = 1
-                    death_time_ = kg.covid_ar[i][4]
-                    self.dic_patient[mrn_single]['death_time'] = death_time_
-                    death_time = death_time_.split(' ')
-                    death_date = [np.int(l) for l in death_time[0].split('-')]
-                    death_date_value = (death_date[0] * 365.0 + death_date[1] * 30 + death_date[2]) * 24 * 60
-                    dead_time_ = [np.int(l) for l in death_time[1].split(':')[0:-1]]
-                    dead_time_value = dead_time_[0] * 60.0 + dead_time_[1]
-                    total_dead_time_value = death_date_value + dead_time_value
-                    self.dic_patient[mrn_single]['death_value'] = total_dead_time_value
-                else:
-                    death_flag = 0
-                self.dic_patient[mrn_single]['death_flag'] = death_flag
+            """
+            filter mortality
+            """
+            if self.covid_ar[i, 4] == self.covid_ar[i, 4]:
+                death_flag = 1
+                death_time_ = kg.covid_ar[i][4]
+                self.dic_patient[mrn_single]['death_time'] = death_time_
+                death_time = death_time_.split(' ')
+                death_date = [np.int(l) for l in death_time[0].split('-')]
+                death_date_value = (death_date[0] * 365.0 + death_date[1] * 30 + death_date[2]) * 24 * 60
+                dead_time_ = [np.int(l) for l in death_time[1].split(':')[0:-1]]
+                dead_time_value = dead_time_[0] * 60.0 + dead_time_[1]
+                total_dead_time_value = death_date_value + dead_time_value
+                self.dic_patient[mrn_single]['death_value'] = total_dead_time_value
+            else:
+                death_flag = 0
+            self.dic_patient[mrn_single]['death_flag'] = death_flag
 
 
         """
@@ -228,102 +226,6 @@ class Kg_construct_ehr():
                     self.total_data_icu.append(i)
                 else:
                     self.un_correct_icu.append(i)
-       index_race = 0
-        for i in self.dic_patient.keys():
-            index_race_ = np.where(self.covid_ar[:, 45] == i)[0]
-            self.check_index = index_race_
-            race = 0
-            for j in index_race_:
-                race_check = self.covid_ar[:, 61][j]
-                if race_check == race_check:
-                    race = race_check
-                    break
-            for j in index_race_:
-                age_check = self.covid_ar[:, 7][j]
-                if age_check == age_check:
-                    age = age_check
-                    break
-            for j in index_race_:
-                gender_check = self.covid_ar[:, 24][j]
-                if gender_check == gender_check:
-                    gender = gender_check
-                    break
-            # self.dic_race['Age']=age
-            # self.dic_race['gender']=gender
-            if race == 0:
-                continue
-            if race[0] == 'A':
-                if 'A' not in self.dic_race:
-                    self.dic_race['A'] = {}
-                    self.dic_race['A']['num'] = 1
-                    self.dic_race['A']['index'] = index_race
-                    index_race += 1
-                else:
-                    self.dic_race['A']['num'] += 1
-                if i not in self.dic_demographic:
-                    self.dic_demographic[i] = {}
-                    self.dic_demographic[i]['race'] = 'A'
-            elif race[0] == 'B':
-                if 'B' not in self.dic_race:
-                    self.dic_race['B'] = {}
-                    self.dic_race['B']['num'] = 1
-                    self.dic_race['B']['index'] = index_race
-                    index_race += 1
-                else:
-                    self.dic_race['B']['num'] += 1
-                if i not in self.dic_demographic:
-                    self.dic_demographic[i] = {}
-                    self.dic_demographic[i]['race'] = 'B'
-            elif race[0] == '<':
-                race_ = race.split('>')[3].split('<')[0]
-                if race_ not in self.dic_race:
-                    self.dic_race[race_] = {}
-                    self.dic_race[race_]['num'] = 1
-                    self.dic_race[race_]['index'] = index_race
-                    index_race += 1
-                else:
-                    self.dic_race[race_]['num'] += 1
-                if i not in self.dic_demographic:
-                    self.dic_demographic[i] = {}
-                    self.dic_demographic[i]['race'] = race_
-            elif race[0] == 'I' or race[0] == 'P':
-                if 'U' not in self.dic_race:
-                    self.dic_race['U'] = {}
-                    self.dic_race['U']['num'] = 1
-                    self.dic_race['U']['index'] = index_race
-                    index_race += 1
-                else:
-                    self.dic_race['U']['num'] += 1
-                if i not in self.dic_demographic:
-                    self.dic_demographic[i] = {}
-                    self.dic_demographic[i]['race'] = 'U'
-            else:
-                if race not in self.dic_race:
-                    self.dic_race[race] = {}
-                    self.dic_race[race]['num'] = 1
-                    self.dic_race[race]['index'] = index_race
-                    index_race += 1
-                else:
-                    self.dic_race[race]['num'] += 1
-                if i not in self.dic_demographic:
-                    self.dic_demographic[i] = {}
-                    self.dic_demographic[i]['race'] = race
-            if 'Age' not in self.dic_race:
-                self.dic_race['Age'] = {}
-                self.dic_race['Age']['index'] = index_race
-                index_race += 1
-            self.dic_demographic[i]['Age'] = age
-            # index_race += 1
-            if 'M' not in self.dic_race:
-                self.dic_race['M'] = {}
-                self.dic_race['M']['index'] = index_race
-                index_race += 1
-            if 'F' not in self.dic_race:
-                self.dic_race['F'] = {}
-                self.dic_race['F']['index'] = index_race
-                index_race += 1
-            self.dic_demographic[i]['gender'] = gender
-
 
         index = 0
         for i in self.dic_patient.keys():
@@ -336,11 +238,11 @@ class Kg_construct_ehr():
             total_value_lab = 0
 
             for k in self.single_patient_lab:
-                obv_id = self.labtest_ar[k][2]
+                obv_id = self.labtest_ar[k][1]
                 patient_lab_mrn = self.labtest_ar[k][0]
-                value = self.labtest_ar[k][3]
-                self.check_data_lab = self.labtest_ar[k][4]
-                date_year_value_lab = float(str(self.labtest_ar[k][4])[0:4]) * 365
+                value = self.labtest_ar[k][2]
+                self.check_data_lab = self.labtest_ar[k][3]
+                date_year_value_lab = float(str(self.labtest_ar[k][3])[0:4]) * 365
                 date_day_value_lab = float(str(self.check_data_lab)[4:6]) * 30 + float(str(self.check_data_lab)[6:8])
                 date_value_lab = (date_year_value_lab + date_day_value_lab) * 24 * 60
                 date_time_value_lab = float(str(self.check_data_lab)[8:10]) * 60 + float(
@@ -368,55 +270,27 @@ class Kg_construct_ehr():
                         self.dic_patient[i]['prior_time_lab'][self.prior_time].setdefault(category, []).append(value)
                     else:
                         self.dic_patient[i]['prior_time_lab'][self.prior_time].setdefault(category, []).append(value)
-            # if not self.dic_lab[category]['patient_values'][i] == {}:
-            #   mean_value_lab_single = np.mean(self.dic_lab[category]['patient_values'][i]['lab_value_patient'])
-            #  self.dic_lab[category]['patient_values'][i]['lab_mean_value']=mean_value_lab_single
 
-            # print(index)
-            # index += 1
-        for j in self.single_patient_vital:
-            obv_id = self.vital_sign_ar[j][2]
-            if obv_id in self.crucial_vital:
-                self.check_data = self.vital_sign_ar[j][4]
-                self.dic_patient[i].setdefault('time_capture', []).append(self.check_data)
-                date_year_value = float(str(self.vital_sign_ar[j][4])[0:4]) * 365
-                date_day_value = float(str(self.check_data)[4:6]) * 30 + float(str(self.check_data)[6:8])
-                date_value = (date_year_value + date_day_value) * 24 * 60
-                date_time_value = float(str(self.check_data)[8:10]) * 60 + float(str(self.check_data)[10:12])
-                total_time_value = date_value + date_time_value
-                self.prior_time = np.int(np.floor(np.float((total_time_value - in_time_value) / 60)))
-                if self.prior_time < 0:
-                    continue
-                if obv_id == 'CAC - BLOOD PRESSURE':
-                    self.check_obv = obv_id
-                    self.check_ar = self.vital_sign_ar[j]
-                    self.check_value_presure = self.vital_sign_ar[j][3]
-                    try:
-                        value = self.vital_sign_ar[j][3].split('/')
-                    except:
+
+            for j in self.single_patient_vital:
+                obv_id = self.vital_sign_ar[j][1]
+                if obv_id in self.crucial_vital:
+                    self.check_data = self.vital_sign_ar[j][3]
+                    self.dic_patient[i].setdefault('time_capture', []).append(self.check_data)
+                    date_year_value = float(str(self.vital_sign_ar[j][3])[0:4]) * 365
+                    date_day_value = float(str(self.check_data)[4:6]) * 30 + float(str(self.check_data)[6:8])
+                    date_value = (date_year_value + date_day_value) * 24 * 60
+                    date_time_value = float(str(self.check_data)[8:10]) * 60 + float(str(self.check_data)[10:12])
+                    total_time_value = date_value + date_time_value
+                    self.prior_time = np.int(np.floor(np.float((total_time_value - in_time_value) / 60)))
+                    if self.prior_time < 0:
                         continue
-                    if self.check_value_presure == '""':
-                        continue
-                    if self.prior_time not in self.dic_patient[i]['prior_time_vital']:
-                        self.dic_patient[i]['prior_time_vital'][self.prior_time] = {}
-                        self.dic_patient[i]['prior_time_vital'][self.prior_time].setdefault('high', []).append(
-                            value[0])
-                        self.dic_patient[i]['prior_time_vital'][self.prior_time].setdefault('low', []).append(
-                            value[1])
-                    else:
-                        self.dic_patient[i]['prior_time_vital'][self.prior_time].setdefault('high', []).append(
-                            value[0])
-                        self.dic_patient[i]['prior_time_vital'][self.prior_time].setdefault('low', []).append(
-                            value[1])
-                    self.dic_vital['high'].setdefault('value', []).append(value[0])
-                    self.dic_vital['low'].setdefault('value', []).append(value[1])
-                else:
-                    self.check_value = self.vital_sign_ar[j][3]
+                    self.check_value = self.vital_sign_ar[j][2]
                     self.check_obv = obv_id
                     self.check_ar = self.vital_sign_ar[j]
                     if self.check_value == '""':
                         continue
-                    value = float(self.vital_sign_ar[j][3])
+                    value = float(self.vital_sign_ar[j][2])
                     if np.isnan(value):
                         continue
                     if self.prior_time not in self.dic_patient[i]['prior_time_vital']:
